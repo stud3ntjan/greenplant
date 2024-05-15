2fbfae0d5a0fa5c82ca2f29980acd71d4e46c21d
stud3ntjan <152269508+stud3ntjan@users.noreply.github.com>
2024-05-15 15:08:39 +0200

    update banner

--- green-plant/src/components/Banner.js
+++ green-plant/src/components/Banner.js
@@ -1,17 +1,33 @@
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+// import headerImg from "../assets/img/soundwave.svg";
 
 export const Banner = () => {
   return (
     <section className="banner" id="home">
       <Container>
         <Row className="align-items-center">
-          {/* <Col xs={12} md={6} xl={7}>
+          <Col xs={12} md={6} xl={7}>
             <span className="tagline">Welcome to my Portfolio</span>
             <h1>
-              {`Hallo test hihi`}
-              <span></span>
+              {`Hey ich bin ein `}
+              <span className="wrap">web deveploper</span>
             </h1>
-          </Col> */}
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+            <button onClick={() => console.log("connect")}>
+              Check mal das
+              <ArrowRightCircle size={25} />
+            </button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Header Img" />
+          </Col>
         </Row>
       </Container>
     </section>
