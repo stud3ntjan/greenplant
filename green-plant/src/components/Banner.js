3089f5c87c0fd84581f36be9696dee4844c52bb4
stud3ntjan <152269508+stud3ntjan@users.noreply.github.com>
2024-05-17 14:46:36 +0200

    add text randomize

--- green-plant/src/components/Banner.js
+++ green-plant/src/components/Banner.js
@@ -1,9 +1,50 @@
+import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg";
 // import headerImg from "../assets/img/soundwave.svg";
 
 export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = ["Web Entwickler", "Web Designer", "Netter Kerl"];
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 200);
+  const period = 500;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
+
   return (
     <section className="banner" id="home">
       <Container>
@@ -12,7 +53,7 @@ export const Banner = () => {
             <span className="tagline">Welcome to my Portfolio</span>
             <h1>
               {`Hey ich bin ein `}
-              <span className="wrap">web deveploper</span>
+              <span className="wrap">{text}</span>
             </h1>
             <p>
               Lorem Ipsum is simply dummy text of the printing and typesetting
