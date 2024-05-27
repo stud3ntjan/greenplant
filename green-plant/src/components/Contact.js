4995e70ffe6394941ad905653cd154a7956f18e8
stud3ntjan <152269508+stud3ntjan@users.noreply.github.com>
2024-05-27 14:44:36 +0200

    create contacts.js

---
+++ green-plant/src/components/Contact.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+ 
+export const Contact = () => {
+    const fromInitialDetails ={
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+        message: '',
+
+    }
+    const [formDetails, setFormDetails] = useState(fromInitialDetails);
+    const [buttonText, setButtonText] = 
+    return (
+
+    )
+}
